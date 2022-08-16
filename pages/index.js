7e3d7dee290dd9e9dce2999e1f69681be94d386e
richardgrif <103006344+richardgrif@users.noreply.github.com>
2022-08-16 14:50:31 -0500

    Admin page setup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
+    const { data: session, status } = useSession()
+
     return (
         <div>
             <Head>
@@ -11,6 +15,7 @@ export default function Home() {
 
             <div className='text-center'>
                 <h1 className='mt-10 font-extrabold text-2xl'>Dread & Brian's Shop</h1>
+                <p className='mt-20'>If you are an Admin, <Link className='text-red-500 font-bold' href={session? 'admin' : 'http://localhost:3000/api/auth/signin'} >Click Here</Link> to login!</p>
             </div>
         </div>
     )
